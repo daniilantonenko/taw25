66d85772fca5ce510756840d94efb6d4bcfcb7c4
PC\da <daniilantonenko@ya.ru>
2025-02-15 17:06:05 +0300

    Fix: entry point fixed

--- Task_2/base_page.py
+++ Task_2/base_page.py
@@ -5,7 +5,7 @@ class BasePage:
 
     def __init__(self, driver):  
         self.driver = driver  
-        self.base_url = "http://tech-avito-intern.jumpingcrab.com/advertisements/"
+        self.base_url = "http://tech-avito-intern.jumpingcrab.com/"
 
     def find_element(self, locator, time=10):  
         return WebDriverWait(self.driver, time).until(EC.presence_of_element_located(locator))
